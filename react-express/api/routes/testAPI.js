30b6b985e1e525e373c2a15205556633f387d447
Thilan Shiroyuki Liyanage <Shiroyuki-Liyanage@users.noreply.github.com>
2020-08-17 22:27:15 +1200

    Deleted Api project, add server js

--- react-express/api/routes/testAPI.js
+++
@@ -1,8 +0,0 @@
-var express = require("express");
-var router = express.Router();
-
-router.get("/", function (req, res, next) {
-  res.send("Api is working properly");
-});
-
-module.exports = router;
