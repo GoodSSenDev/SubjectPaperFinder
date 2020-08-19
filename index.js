e4be54e87dbc52cec4bc843e91f51c84d00312ff
GoodSSenDev <yoodonggeun7@naver.com>
2020-08-19 17:05:28 +1200

    Heroku Deploy setting package setting

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const paperController = require('./controllers/papersController') 
-
+const path = require('path');
 const PORT = process.env.PORT || 5000;
 var app = express()
+
+
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.get('/', function (req, res) {
   res.send('<h1>Hello world</h1>')
@@ -12,5 +15,9 @@ app.get('/data', (req, res) => {
   const papers = paperController.getPapers()
   papers.then((paperData) => res.send(paperData))
 })
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
 
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
