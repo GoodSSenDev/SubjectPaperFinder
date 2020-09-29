5d610ef066d4a563fd85c707267ea67d86d7adb5
lcerte <zhangyunxinnikeneo@gmail.com>
2020-09-29 16:57:50 +0800

    adding sortingbydata buttoms

---
+++ sortingbydata_ascending.js
@@ -0,0 +1,30 @@
+var ul = document.getElementsByTagName('ul')[0];
+var li = ul.getElementsByTagName('li');
+var list = [];
+
+
+function fun1(ul, li, list) {
+    for (var i = 0; i < li.length; i++) {
+        var tmp = {};
+        tmp.dom = li[i];
+        tmp.date = new Date(li[i].firstChild.data.replace(/-/g, '/'));
+        list.push(tmp);
+    }
+    list.sort(function(a, b) {
+        return b.date - a.date; //a.date - b.date;
+    });
+    for (var i = 0; i < list.length; i++) {
+        ul.appendChild(list[i].dom);
+    }
+}
+
+function fun2() {
+    alert('hi');
+}
+
+//1.get object of the 'sort'
+var sort = document.getElementsByClassName("sort1");
+//2.event
+sort[0].onclick = function() {
+    fun1(ul, li, list);
+}
