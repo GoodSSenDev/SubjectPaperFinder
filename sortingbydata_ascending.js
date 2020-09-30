2739d5ef66b6a3939acddea2bf9d59c9c13bd655
lcerte <zhangyunxinnikeneo@gmail.com>
2020-09-30 19:06:38 +0800

    change

--- sortingbydata_ascending.js
+++ sortingbydata_ascending.js
@@ -17,40 +17,40 @@
 // }];
 
 module.exports = {
-  //selection sort
-  ascendingSort: function (arr) {
-    var len = arr.length;
-    var earlyIndex, temp;
-    var date1 = [];
-    var date2 = [];
-    for (var i = 0; i < len - 1; i++) {
-      date1 = arr[i].date.split("/"); // ["01", "00", "2020"]
+    //selection sort
+    ascendingSort: function(arr) {
+        var len = arr.length;
+        var earlyIndex, temp;
+        var date1 = [];
+        var date2 = [];
+        for (var i = 0; i < len - 1; i++) {
+            date1 = arr[i].date.split("/"); // ["01", "00", "2020"]
 
-      earlyIndex = i;
+            earlyIndex = i;
 
-      for (var j = i + 1; j < len; j++) {
-        date2 = arr[j].date.split("/");
+            for (var j = i + 1; j < len; j++) {
+                date2 = arr[j].date.split("/");
 
-        //compare year
-        if (date1[2] < date2[2]) {
-          earlyIndex = j;
-        } else {
-          // compare month
-          if (date1[1] < date2[1]) {
-            earlyIndex = j;
-          } else {
-            //compare day
-            if (date1[0] < date2[0]) {
-              earlyIndex = j;
+                //compare year
+                if (date1[2] < date2[2]) {
+                    earlyIndex = j;
+                } else {
+                    // compare month
+                    if ((date2[2] == date1[2]) && (date1[1] < date2[1])) {
+                        earlyIndex = j;
+                    } else {
+                        //compare day
+                        if ((date2[2] == date1[2]) && (date2[1] == date1[1]) && (date1[0] < date2[0])) {
+                            earlyIndex = j;
+                        }
+                    }
+                }
             }
-          }
-        }
-      }
 
-      temp = arr[i];
-      arr[i] = arr[earlyIndex];
-      arr[earlyIndex] = temp;
-    }
-    return arr;
-  },
+            temp = arr[i];
+            arr[i] = arr[earlyIndex];
+            arr[earlyIndex] = temp;
+        }
+        return arr;
+    },
 };
