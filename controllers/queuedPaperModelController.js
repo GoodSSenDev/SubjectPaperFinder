ae2ae5f363ceadcac48a49d58eddae82fd69469e
GoodSSenDev <yoodonggeun7@naver.com>
2020-10-01 15:08:18 +1300

    Added controller so queued paper model can be used

---
+++ controllers/queuedPaperModelController.js
@@ -0,0 +1,48 @@
+const queuedPaperModel = require('../models/queuedPaperModel')
+
+//this class is singleton class and returns own instance
+//this class useses queuedPaperModels 
+export class queuedPaperModelController {
+
+    constructor() {
+        if (queuedPaperModelController.exist) {
+            return queuedPaperModelController.instance;
+        }
+
+        this.papers = [];
+
+        this._queuedPaperModel = queuedPaperModel;
+
+        queuedPaperModelController.instance = this;
+
+        queuedPaperModelController.exist = true;
+
+        return this;
+    }
+
+    async init() {
+        this.papers = await queuedPaperModel.getQueuedPapers();
+    }
+
+    getTags() {
+        return this.papers;
+    }
+    async insertNewPaper(paper) {
+        //if insertNewPaper success it will return true
+        if (await queuedPaperModel.insertNewQueuedPaper(paper)) {
+            const { _PId } = this.papers[this.papers.lastIndexOf()];
+            this.papers.push({ _PId: _PId + 1, ...paper });
+        }
+        return this.papers;
+    }
+
+
+    async deleteTagById(PId) {
+        //if insertNewPaper success it will return true
+        if (await queuedPaperModel.deleteQueuedPaperId(PId)) {
+            this.papers = this.papers.filter(x => x._PId !== _PId);
+        }
+        return this.papers;
+    }
+
+} 
