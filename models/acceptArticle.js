5c511a83b69f16bccadebb5fcdf1e53ef8fb8ae0
unknown <howardmao333@gmail.com>
2020-10-07 20:57:32 +1300

    added acceptedPapersModel.js

---
+++ models/acceptArticle.js
@@ -0,0 +1,4 @@
+require("./connectMongo");
+const acceptedModel = require("./acceptedPapersModel").acceptedModel;
+
+async function moveArticleToAccepted() {}
---
+++ models/acceptedPapersModel.js
@@ -0,0 +1,21 @@
+const mongoose = require("./connectMongo").mongoose;
+
+var Schema = mongoose.Schema;
+const acceptedSchema = new Schema({
+  _PId: { type: Number, required: true },
+  author: { type: String, required: false },
+  title: { type: String },
+  journal: { type: String, required: false },
+  year: { type: Number, required: false },
+  pages: { type: String, required: false },
+  month: { type: String, required: false },
+  annote: { type: Number, required: false },
+  eprint: { type: String, required: false },
+  eprinttype: { type: String, required: false },
+  eprintclass: { type: String, required: false },
+});
+
+const Model = mongoose.model;
+const acceptedModel = Model("acceptedpapers", acceptedSchema);
+
+module.exports.acceptedModel = acceptedModel;
