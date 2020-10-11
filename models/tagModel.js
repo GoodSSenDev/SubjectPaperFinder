316c8281f6ec5c1bb9067c5157013a2ab6f18f3c
GoodSSenDev <yoodonggeun7@naver.com>
2020-10-11 23:23:16 +1300

    Added tag Model
    For using tag data schema to connect mongoose

---
+++ models/tagModel.js
@@ -0,0 +1,14 @@
+const mongoose = require("./connectMongo.js").mongoose;
+
+const Schema = mongoose.Schema;
+
+const Tag = new Schema({
+    ID: Number,
+    Name: String,
+    Level: Number,
+    created: { type: Date, default: Date.now }
+});
+
+
+module.exports.mongoose = mongoose;
+module.exports = mongoose.model('tags', Account);
